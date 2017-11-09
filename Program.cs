f3822959e5156f5dba5e4baac5dc10a0e8d4e701
toby2o12 <toby2o12@hotmail.com>
2017-11-09 19:53:34 +0800

    添加dapper

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Dapper;
+
+namespace dotnetcoretest {
+    class Program {
+        static void Main (string[] args) {
+            string connString = "";
+            using (var conn = new MySql.Data.MySqlClient.MySqlConnection ("server=192.168.255.128;User Id=root;password=root;Database=testdb"))
+            {
+                conn.Open();
+                string sql="select count(1) from tb_user";
+                int count=conn.QueryFirst<int>(sql);
+            }
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
